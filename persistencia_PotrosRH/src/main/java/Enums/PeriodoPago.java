254bf163d36e7875003e624c8022494cc9d90f96
Leonardo Flores Leyva <87454042+leonardofloresleyva@users.noreply.github.com>
2025-05-22 13:26:45 -0700

    Clase de inserción de empleados lista
    Se sube una clase de prueba en DAO, para la inserción de 6 empleados de prueba, de los cuales 2 se pueden probar en la generación de nóminas.

--- persistencia_PotrosRH/src/main/java/Enums/PeriodoPago.java
+++ persistencia_PotrosRH/src/main/java/Enums/PeriodoPago.java
@@ -1,15 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
- */
 package Enums;
 
 /**
- *
+ * Períodos de pago de una nómina.
  * @author Jose Luis Islas Molina 252574
  */
 public enum PeriodoPago {
     SEMANAL,
-    QUIINCENAL,
+    QUINCENAL,
     MENSUAL
 }
